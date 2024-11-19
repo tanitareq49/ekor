5b78b154592c87b298882262b7ed6fa2782bc745
Habibur Rahman <hrsshainik@gmail.com>
2024-11-20 03:59:14 +0600

    ekor project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'barlow': ['Barlow', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'green-800': '#1D7460',
+        'green-600': '#22826C',
+        'green-200': '#519888',
+        'green-100': '#E8FAF6',
+        'orange': '#F16520',
+        'orange-100': '#FEEFE8',
+        'black': '#2A3741',
+        'white': '#FFFFFF',
+        'grey-200': '#F4F5F6',
+        'grey-400': '#ECECEC'
+      },
+    },
+  },
+  plugins: [],
+}
+
