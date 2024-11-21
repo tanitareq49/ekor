638977f0d0954372d6402e19cec7e83648aa53f3
Habibur Rahman <hrsshainik@gmail.com>
2024-11-22 01:40:40 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,15 +20,17 @@ module.exports = {
         'grey-400': '#ECECEC'
       },
       screens: {
+        'xs': '450px',
+        
         'sm': '640px',
 
-        'md': '768px',
+        'md': '800px',
 
         'lg': '1024px',
 
         'xl': '1220px',
 
-        '2xl': '1300',
+        '2xl': '1300px',
 
         '3xl': '1420px'
       }
